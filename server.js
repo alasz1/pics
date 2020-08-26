b6ad10d81c45cfac7c1c6c2a7dae81946e05707e
alasz <antti.laszlo@gmail.com>
2020-08-26 23:25:36 +0300

    first c

---
+++ server.js
@@ -0,0 +1,32 @@
+// Server returning array of file names in selected directory
+
+const express = require('express')
+const fs = require('fs');
+const path = require('path');
+const app = express()
+const cors = require('cors');
+const port = 3000
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  // directory will be selected by request query parameter "dir"
+  console.log("req.query: ", req.query.dir)
+  const dirPath = path.join(__dirname, "public", "assets", req.query.dir);
+  fs.readdir(dirPath, function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    //push all files in selected dir into array
+    files.forEach(file => {
+      files.concat(file)
+    });
+    console.log("files: ", files)
+    res.send(files)
+  });
+})
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
